09afe60cf8b54cd1ae0b7c244d1ef9ed95e2ced2
miabartl <32626386+miabartl@users.noreply.github.com>
2017-11-01 10:36:49 -0700

    Add files via upload

---
+++ ECProgram1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void) {
+	
+	int x;
+	
+	printf("Please enter an integer: ");
+	scanf("%d", &x);
+	
+	printf("%d\n", x);
+	
+	return 0;
+}
---
+++ ECProgram2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void) {
+
+	float x;
+
+	printf("Please enter a value: ");
+	scanf("%f", &x);
+
+	printf("%f\n", x);
+
+	return 0;
+}
